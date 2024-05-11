2daee8510323b617695be6b74e69dbf4ec262f42
kaushik-2318 <kaushikverma321@gmail.com>
2024-05-11 21:59:54 +0530

    Edit Post is Done

--- app.js
+++ app.js
@@ -90,6 +90,30 @@ app.post('/createpost', isLoggedIn,async(req, res)=>{
     res.status(200).redirect("/profile");
 })
 
+app.get('/like/:id', isLoggedIn, async (req, res)=>{
+    let post = await postModel.findOne({_id: req.params.id}).populate("user")
+
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid);
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+    await post.save();
+    res.status(200).redirect("/profile");
+})
+
+app.get('/edit/:id', isLoggedIn, async (req, res)=>{
+    let post = await postModel.findOne({_id: req.params.id}).populate("user")
+
+    res.render("edit",{post});
+})
+
+app.post('/update/:id', isLoggedIn, async (req, res)=>{
+    let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content})
+    res.redirect("/profile");
+})
+
 function isLoggedIn(req, res, next){
     if (req.cookies.token === "") res.redirect("/login");
     else{
